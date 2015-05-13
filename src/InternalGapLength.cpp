be21746c1e10da97e066f325334a11deef0190d9
Adriano Baćac <adrianobacac@gmail.com>
2015-05-13 10:44:58 +0200

    internal gap inclusion rule added

---
+++ src/InternalGapLength.cpp
@@ -0,0 +1,44 @@
+//
+// Created by ppx on 13.05.15..
+//
+
+#include "InternalGapLength.h"
+#include <cassert>
+
+InternalGapLength::~InternalGapLength() {
+
+}
+
+virtual void InternalGapLength::preprocess(Seq *seq, Seq *cons){
+    this->_seq = seq;
+    this->_cons = cons;
+    this-> _gap_length = 0;
+    this-> _temp_gap_length = 0;
+    this-> _was_first_match = false;
+
+}
+
+virtual void InternalGapLength::process(Node *node){
+    assert(_seq != nullptr && _cons != nullptr && node->hasSeq(this->_seq));
+    if (!node->hasSeq(_cons) && !_was_first_match){
+        return;
+    }else if(node->hasSeq(_cons) && !_was_first_match){
+        _was_first_match = true;
+    }else if(!node->hasSeq(_cons) && _was_first_match){
+        _temp_gap_length++;
+    }else if(node->hasSeq(_cons) && _was_first_match){
+        _gap_length += _temp_gap_length;
+        _temp_gap_length = 0;
+    }
+
+}
+
+virtual bool InternalGapLength::result(){
+    assert(_gap_length >= 0 && _limit >= 0);
+    return _gap_length <= _limit;
+
+}
+
+InternalGapLength::InternalGapLength(int limit):_limit(limit), _gap_length(0), _temp_gap_length(0), _was_first_match(false) {
+    assert(limit >= 0);
+}
---
+++ src/InternalGapLength.h
@@ -0,0 +1,33 @@
+//
+// Created by ppx on 13.05.15..
+//
+
+#ifndef POA_INTERNALGAPLENGHT_H
+#define POA_INTERNALGAPLENGHT_H
+
+
+#include "InclusionRule.h"
+
+class InternalGapLength: public InclusionRule {
+public:
+    int _limit;
+    int _gap_length;
+    int _temp_gap_length;
+
+    bool _was_first_match;
+
+    virtual ~InternalGapLength();
+
+
+    InternalGapLength(int limit);
+
+    virtual void preprocess(Seq *seq, Seq *cons);
+
+    virtual void process(Node *node);
+
+    virtual bool result();
+
+};
+
+
+#endif //POA_INTERNALGAPLENGHT_H
